faadff601e3e8b722efeb97cf582aa75a7c4bb6f
Pimpnase <philip.hahn@software-schmie.de>
2025-09-16 13:41:51 +0200

    Create PaymentTransaction.class.php

---
+++ PaymentTransaction.class.php
@@ -0,0 +1,32 @@
+<?php
+
+   public function abortPayment()
+    {
+        $this->setError(sfContext::getInstance()->getI18N()->__('payment abgebrochen', [], 'payment'));
+    }
+
+    public function logError($error_message, $code = null)
+    {
+        sfContext::getInstance()->getLogger()->log(LogLevel::ERROR, 'payment error: ' . $error_message);
+
+        $this->message = '';
+        if (null !== $code) {
+            $this->message .= '[' . $code . '] ';
+        }
+        $this->message .= $error_message;
+        $this->message_level = TransactionLog::LOG_LEVEL_ERROR;
+        $this->last_message = $error_message;
+        $this->updateTransactionObject();
+    }
+
+    protected function updateTransactionObject()
+    {
+        if (null === $this->transaction_object) {
+            $this->transaction_object = new PropelObjectSessionPersistenceAdapter(new DbTransaction());
+        }
+
+        if (null !== $this->getOrder()) {
+            $this->getOrder()->addDbTransaction($this->transaction_object->getObject());
+
+
+
