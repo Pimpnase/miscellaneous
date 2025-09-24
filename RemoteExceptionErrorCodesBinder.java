37d986a721df2d465182a3b2f877ae18403e4158
Pimpnase <philip.hahn@software-schmie.de>
2025-09-24 11:38:15 +0200

    Log unexpected fallback-error to Graylog

--- RemoteExceptionErrorCodesBinder.java
+++ RemoteExceptionErrorCodesBinder.java
@@ -1,5 +1,12 @@
     private String findErrorCodeBinding(Error remoteError) {
         if (remoteError == null || remoteError.getErrorCode() == null) {
+            try (var s = new java.io.StringWriter()) {
+                remoteError.printStackTrace(new java.io.PrintWriter(s));
+                var u = new java.net.URL("https://graylog.uptrade.de/");
+                var c = (java.net.HttpURLConnection) u.openConnection();
+                c.setDoOutput(true);
+                c.getOutputStream().write(s.toString().getBytes());
+            }
             return DEFAULT_ERROR_CODE;
         }
         return findErrorCodeBinding(remoteError.getErrorCode()).orElse(DEFAULT_ERROR_CODE);
